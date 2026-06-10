50accf8769c9f03e1b066027773982c8d3d9ffb4
nmajufavour16 <nmajufavour16@gmail.com>
2026-06-10 19:23:34 +0100

    added descriptional comments

--- timetable.py
+++ timetable.py
@@ -1,6 +1,9 @@
-# Multiplication table of numbers from 2 to 12
-for num in range(2, 13):
+# Program to print multiplication table of numbers from 2 to 12
+
+for num in range(2, 13): # This checks for the numbers from 2 and 12
     print(f"Multiplication table for {num}:")
+    
+    # This code block takes each number within the range above and multiplies with the range below
     for n in range(1, 13):
         product = num * n
         print(f"{num} x {n} = {product}")
